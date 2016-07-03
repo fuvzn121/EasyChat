51998036f30dd96bd189c833618f11b723a5f024
hrys121 <c011426930@edu.teu.ac.jp>
2016-07-04 06:34:41 +0900

    プロジェクト ファイルを追加します。

---
+++ EasyChat/Hubs/EchoHub.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNet.SignalR;
+using Microsoft.AspNet.SignalR.Hubs;
+
+namespace EasyChat.Hubs {
+    [HubName("Echo")]
+    public class EchoHub : Hub {
+        public void Hello() {
+            Clients.All.hello();
+        }
+        public void Send(String text) {
+            Clients.All.Receive(text);
+        }
+    }
+}
